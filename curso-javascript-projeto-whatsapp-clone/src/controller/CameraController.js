f7cd7bd5c23eafb6ba60f0e779426d3678499d0a
joao-souza-flooxai <joao.souza.flooxai@gmail.com>
2025-03-28 12:10:45 -0300

    feat: Added the events on Camera panel and begining the construction for the image to be sent.

--- curso-javascript-projeto-whatsapp-clone/src/controller/CameraController.js
+++ curso-javascript-projeto-whatsapp-clone/src/controller/CameraController.js
@@ -15,11 +15,27 @@ export class CameraController{
         });
     }
 
+    //Parando tudo
     stop(){
         
         this._stream.getTracks().forEach(track => {
             track.stop();
         });
+    }
+
+    //Desenhado a imagem com canvas
+    takePicture(mimeType = 'image/png'){
+
+        let canvas = document.createElement('canvas');
+        canvas.setAttribute('height', this._videoEl.videoHeight);
+        canvas.setAttribute('width', this._videoEl.videoWidth);
+
+        let context = canvas.getContext('2d');
+
+        context.drawImage(this._videoEl, 0,0, canvas.width, canvas.height);
+
+        return canvas.toDataURL(mimeType);
+        
     }
 
 
